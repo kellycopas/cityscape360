08705170dcbec8a94357c3205eb0a88107078de3
kellycopas <39497444+kellycopas@users.noreply.github.com>
2019-03-07 11:38:42 +0000

    Delete scroll.js

--- scroll.js
+++
@@ -1,8 +0,0 @@
-// code based on: https://codepad.co/snippet/YPDXE33r
-
-$(function() {
-    $('#arrow').click (function() {
-      $('html, body').animate({scrollTop: $('div.section1').offset().top }, 'slow');
-      return false;
-    });
-  });
